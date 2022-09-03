82e2872b5ed10f53296f958072d2d04ed270255e
Son Hong Su <son.ho463@gmail.com>
2022-09-03 11:23:35 +0900

    form

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,12 +1,10 @@
-
-
 // Swiper.js
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
   slidesPerView: 1.3,
-	centeredSlides: true,
+  centeredSlides: true,
   spaceBetween: 50,
   speed: 1000,
   autoplay: false,
@@ -35,3 +33,17 @@ const swiper = new Swiper(".swiper", {
     draggable: true,
   },
 });
+
+// サービスブロックのアイテムオープン
+
+const servicesItem = document.getElementsByClassName("js-services__item");
+console.log(servicesItem);
+for (let i = 0; i < servicesItem.length; i++) {
+  servicesItem[i].addEventListener("click", function () {
+		console.log('click');
+		console.log(servicesItem[i].classList.contains('open'));
+    servicesItem[i].classList.contains("open")
+      ? servicesItem[i].classList.remove("open")
+      : servicesItem[i].classList.add("open");
+  });
+}
