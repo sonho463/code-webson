5e96d2f2dffc6b1991244aa3a90ca90e1243ed8d
Son Hong Su <son.ho463@gmail.com>
2022-09-04 11:04:45 +0900

    レスポンシブ worksブロック swiper.js

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -3,19 +3,18 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
-  slidesPerView: 1.3,
+  slidesPerView: 1.1,
   centeredSlides: true,
-  spaceBetween: 50,
+  spaceBetween: 30,
   speed: 1000,
   autoplay: false,
-  // effect: "fade",
-  // breakpoints: {
-  // 	768: {
-  // 		slidesPerView: 3.5,
-  // 		// spaceBetween: 020
-  // 	},
-  // },
-
+  breakpoints: {
+		// スライドの表示枚数：500px以上の場合
+    769: {
+			slidesPerView: 1.3,
+			spaceBetween: 50,
+    }
+  },
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
