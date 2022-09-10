17b0e11ac6a0b1ad9c3cfc878f01aa8d854aaa60
Son Hong Su <son.ho463@gmail.com>
2022-09-11 07:00:42 +0900

    アバウトのモーダルの挙動OK

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -38,19 +38,18 @@ const swiper = new Swiper(".swiper", {
 const servicesItem = document.getElementsByClassName("js-services__item");
 console.log(servicesItem);
 for (let i = 0; i < servicesItem.length; i++) {
-	const button = servicesItem[i].querySelector('.js-services-button')
-	const buttonText = servicesItem[i].querySelector('.js-services-button__text')
+  const button = servicesItem[i].querySelector(".js-services-button");
+  const buttonText = servicesItem[i].querySelector(".js-services-button__text");
   button.addEventListener("click", function () {
     console.log(servicesItem[i].classList.contains("open"));
 
-		if(servicesItem[i].classList.contains("open")){
-			servicesItem[i].classList.remove("open")
-			buttonText.textContent = '詳しく';
-
-		} else {
-			servicesItem[i].classList.add("open");
-			buttonText.textContent = '閉じる';
-		}
+    if (servicesItem[i].classList.contains("open")) {
+      servicesItem[i].classList.remove("open");
+      buttonText.textContent = "詳しく";
+    } else {
+      servicesItem[i].classList.add("open");
+      buttonText.textContent = "閉じる";
+    }
     servicesItem[i].scrollIntoView();
   });
 }
@@ -66,3 +65,72 @@ for (let i = 0; i < servicesItem.length; i++) {
 //     servicesItem[i].scrollIntoView();
 //   });
 // }
+
+// ページ内リンクの設定
+
+const setLinkInPage = (targetItemList) => {
+	targetItemList.forEach((item) => {
+		item.addEventListener("click", (event) => {
+			console.log(event);
+			event.preventDefault();
+			const targetId = event.target.hash;
+			const target = document.querySelector(targetId);
+			console.log(target)
+			target.scrollIntoView({ behavior: "smooth" });
+		});
+	});
+}
+
+const navItems = document.querySelectorAll(".p-g-nav__item > a");
+const navNext = document.querySelectorAll('.c-arrow__next');
+const linkButton = document.querySelectorAll('.js-c-button');
+setLinkInPage(navItems);
+setLinkInPage(navNext);
+setLinkInPage(linkButton);
+// navItems.forEach((item) => {
+//   item.addEventListener("click", (event) => {
+//     console.log(event);
+//     event.preventDefault();
+//     const targetId = event.target.hash;
+//     const target = document.querySelector(targetId);
+// 		console.log(target)
+//     target.scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+// navNext.forEach((item) => {
+//   item.addEventListener("click", (event) => {
+//     console.log(event);
+//     event.preventDefault();
+//     const targetId = event.target.hash;
+//     const target = document.querySelector(targetId);
+// 		console.log(target)
+//     target.scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+// linkButton.forEach((item) => {
+//   item.addEventListener("click", (event) => {
+//     console.log(event);
+//     event.preventDefault();
+//     const targetId = event.target.hash;
+//     const target = document.querySelector(targetId);
+// 		console.log(target)
+//     target.scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+// アバウトページのモーダル開閉
+
+const modal = document.querySelector('.p-about__modal')
+const buttonCloseModal = document.querySelector('.p-modal__close')
+const buttonOpenModal = document.querySelector('.p-about__open')
+const aboutArrowNext = document.querySelector('.p-about > .c-arrow')
+
+const closeModal = () => {
+	modal.classList.remove('open')
+	aboutArrowNext.style.display="block"
+}
+const  openModal = () => {
+	modal.classList.add('open')
+	aboutArrowNext.style.display="none"
+}
